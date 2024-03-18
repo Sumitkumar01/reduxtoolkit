2e4b381795e7ab1da5abdac141646a340486d97d
sumit <sk7627577@gmail.com>
2024-03-18 19:44:09 +0530

    redux-toolkit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddTodo from "./component/AddTodo";
+import Todo from "./component/Todo";
+
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
